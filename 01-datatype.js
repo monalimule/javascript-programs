2b48932904de1ecb7d9ed8c4b8e30e4ea984aac5
monalimule <monalimule1408@gmail.com>
2024-07-31 19:07:22 +0530

    datatypes-variable design
    datatypes-variable design

---
+++ 01-datatype.js
@@ -0,0 +1,50 @@
+// Primitive Datatype: Number, String, Boolean
+console.log("================== Number Datatype================");
+var age=22;
+console.log("Age is:", age);
+var ageTypeof  = typeof age;
+console.log("Datatype of age is:",ageTypeof);
+
+console.log("================== String Datatype================");
+var city='Solapur'
+console.log("City is:", city);
+var cityTypeof = typeof city;
+console.log("Datatype of city is:",cityTypeof);
+
+console.log("================== String Datatype================");
+var marks="99";
+console.log("Marks is:", marks);
+var marksTypeof = typeof marks;
+console.log("Datatype of marks is:", marksTypeof );
+
+console.log("================== Boolean Datatype================");
+var isMarried = true;
+console.log("isMarried:",isMarried);
+var isMarriedTypeof = typeof isMarried;
+console.log("Datatype od isMarried is:",isMarriedTypeof);
+
+var isFailed = false;
+console.log("isFailed :",isFailed );
+var isFailedTypeof = typeof isFailed ;
+console.log("Datatype od isFailed  is:",isFailedTypeof );
+
+
+console.log("================== Special Datatype: Undefined ================");
+var country;
+console.log("Country value is:", country);
+var countryTypeof = typeof country;
+console.log("Datatype of country is:",countryTypeof );
+
+
+console.log("================== Special Datatype:Null ================");
+var village = null;
+console.log("Village value is:",village);
+var villageTypeof = typeof village;
+console.log("Datatype of village is:",villageTypeof );
+
+
+
+
+
+
+
---
+++ 02-datatype.js
@@ -0,0 +1,36 @@
+console.log("======================== Task 1 =========================");
+
+// Variable Declaration 
+var myFullName;
+console.log(myFullName);
+
+// Variable Initialization
+myFullName = "Monali Mule";
+console.log(myFullName);
+
+// Variable Updates
+myFullName = "Monali Rajkumar Mule";
+console.log(myFullName);
+
+console.log("======================== Task 2 =========================");
+var myLove = "I love only JS"
+console.log("I love only JS");
+
+console.log("======================== Task 3 =========================");
+console.log("First Name is: Monali");
+
+console.log("Last NAme is: Mule");
+
+console.log("Age is: 22");
+
+console.log("My Address: Saste Complex,Pune");
+
+console.log("Pincode is: 412105");
+
+console.log("Gender is: Female");
+
+console.log("isMarried: false");
+
+
+
+
