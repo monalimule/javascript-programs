f440d39b839a112a89f74bf97569839b7208c9fe
monalimule <monalimule1408@gmail.com>
2024-07-31 17:38:46 +0530

    javascript variables
    javascript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,10 @@
+
+console.log("My city name is: Solapur");
+
+console.log("My best friend name is: Shravani and Snehita");
+
+console.log("My country name is: India");
+
+console.log("My school name is: BMP Kanya Prashala");
+
+console.log("My college name is: DAV Velankar COllege");
