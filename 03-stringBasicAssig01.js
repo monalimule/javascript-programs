9f222d8709ce1ef7379eaf6fda4b4a0b55812e44
monalimule <monalimule1408@gmail.com>
2024-08-03 19:33:04 +0530

    string basics1
    string basics1

---
+++ 03-stringBasicAssig01.js
@@ -0,0 +1,14 @@
+
+
+function stringBasics() {
+    console.log(`My dream company is: "Infosys" ` );
+    var hobbyOne = "Folk dance"
+    var hobbyTwo = "Treaking"
+    var hobbyThree = "Programming"
+
+    console.log(`My hobbies are: ${hobbyOne}, ${ hobbyTwo } , ${hobbyThree}`);
+
+    
+    console.log("Total number of characters is: ",hobbyOne.length + hobbyTwo.length + hobbyThree.length);
+}
+stringBasics("Folk dance", "Treaking", "Programming");
